e83b2b45a172c6766fa4bb5cb7f20a957eb888a6
Jill0001 <1465514267@qq.com>
2020-12-08 10:52:04 +0800

    train split val

--- train_lmks_split.py
+++ train_lmks_split.py
@@ -87,10 +87,15 @@ def eval_net(net, loader, device):
     c_tp = 0
     t_right = 0
     t_tp = 0
+    all_right = 0
+    all_tp=0
     c_result = []
     c_label = []
     t_result = []
     t_label = []
+    all_result=[]
+    all_label=[]
+
 
     with tqdm(total=n_val, desc='Validation round', unit='batch', leave=False) as pbar:
         for batch in loader:
@@ -105,6 +110,8 @@ def eval_net(net, loader, device):
             with torch.no_grad():
                 c_out, t_out, mf_out = net(input_v, input_a, input_t)
 
+            all_out = c_out*t_out
+
 
             # c_out = torch.squeeze(c_out, dim=1)
             c_result.append(bool(c_out[0][0].cpu() <= c_out[0][1].cpu()))
@@ -112,8 +119,8 @@ def eval_net(net, loader, device):
             t_result.append(bool(t_out[0][0].cpu() <= t_out[0][1].cpu()))
             t_label.append(bool(text_label))
 
-            # all_result.append(bool((all_out[0][0].cpu() <= all_out[0][1].cpu())))
-            # all_label.append(bool(va_label * text_label))
+            all_result.append(bool((all_out[0][0].cpu() <= all_out[0][1].cpu())))
+            all_label.append(bool(va_label * text_label))
 
             pbar.update()
     for j in range(len(c_result)):
@@ -125,28 +132,28 @@ def eval_net(net, loader, device):
             t_right += 1
             if t_result[j]:
                 t_tp += 1
-        # if all_result[j] == all_label[j]:
-        #     all_right += 1
-        #     if all_result[j]:
-        #         all_tp += 1
+        if all_result[j] == all_label[j]:
+            all_right += 1
+            if all_result[j]:
+                all_tp += 1
     print(c_right,t_right,len(c_result))
     print(c_tp, t_tp, all_tp)
-    # print(all_right, len(all_result), all_right / len(all_result))
+    print(all_right, len(all_result), all_right / len(all_result))
     p_c = c_tp / (c_result.count(True) + 1e-6)
     p_t = t_tp / (t_result.count(True) + 1e-6)
-    # p_all = all_tp / (all_result.count(True) + 1e-6)
+    p_all = all_tp / (all_result.count(True) + 1e-6)
 
     r_c = c_tp / (c_label.count(True) + 1e-6)
     r_t = t_tp / (t_label.count(True) + 1e-6)
-    # r_all = all_tp / (all_label.count(True) + 1e-6)
+    r_all = all_tp / (all_label.count(True) + 1e-6)
 
     F1_c = (2 * p_c * r_c) / (p_c + r_c+ 1e-6)
     F1_t = (2 * p_t * r_t) / (p_t + r_t+ 1e-6)
-    # F1_all = (2 * p_all * r_all) / (p_all + r_all + 1e-6)
+    F1_all = (2 * p_all * r_all) / (p_all + r_all + 1e-6)
 
     print("precision_c: %f\nrecall_c: %f\nF1_c: %f\n" % (p_c, r_c, F1_c))
     print("precision_t: %f\nrecall_t: %f\nF1_t: %f\n" % (p_t, r_t, F1_t))
-    # print("precision_all: %f\nrecall_all: %f\nF1_all: %f\n" % (p_all, r_all, F1_all))
+    print("precision_all: %f\nrecall_all: %f\nF1_all: %f\n" % (p_all, r_all, F1_all))
 
     net.train()
 
